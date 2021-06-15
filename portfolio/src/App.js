b82fc38f1a5a920fc6907885cacbc0d1118ddc06
Vasudev <vasudev.pikkili@gmail.com>
2021-06-15 23:54:48 +0530

    Project Setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <h2>My Portfolio</h2>
+    </div>
+  );
+}
+
+export default App;
